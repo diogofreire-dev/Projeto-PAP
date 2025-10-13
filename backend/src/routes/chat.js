d38503488b8e2ccd86c79cab7543a5c99a315017
Diogo Freire <diogoffreire1@gmail.com>
2025-10-13 18:03:24 +0100

    Apaguei o conteúdo que criámos até agora pois iremos realizar outro tipo de projeto para esta atividade.

--- backend/src/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Enviar mensagem
-router.post('/:cloneId', (req, res) => {
-  res.json({ response: 'Olá! Sou o teu clone virtual.' });
-});
-
-module.exports = router;
--- base de dados/4_creat_tabel_conversas.sql
+++
@@ -1,8 +0,0 @@
-use PAP;
-CREATE TABLE Conversations (
-    id VARCHAR(36) PRIMARY KEY,
-    clone_id VARCHAR(36) NOT NULL,
-    messages JSON NOT NULL,  -- guarda mensagens em formato JSON
-    last_message_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
- CONSTRAINT fk_conversations_clone FOREIGN KEY (clone_id) REFERENCES Clones(id) ON DELETE CASCADE;
