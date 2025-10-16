1030bd72c5fac67ccd29a00fb7e85bf5a8fe540c
diogofreire-dev <diogoffreire1@gmail.com>
2025-10-16 10:12:46 +0100

    Fiz queries pa calcular percentagens e Limites.

--- base_dados/queries.sql
+++ base_dados/queries.sql
@@ -47,6 +47,7 @@ SELECT MIN(valor) AS gasto_minimo
     FROM transactions
     WHERE card_id = 1;
 
+#---Agrupar dados por categoria---
 # Toltal gasto por categoria
 SELECT categoria, SUM(valor) AS total_gasto
     FROM transactions
@@ -70,3 +71,39 @@ SELECT categoria, AVG(valor) AS media, COUNT(*) AS quantidade
 SELECT DISTINCT categoria
     FROM transactions
     WHERE card_id = 1;
+
+#---Calcular Percentagens e Limites---
+# Ver limite e o total gasto
+SELECT c.nome_cartao, c.limite, COALESCE(SUM(t.valor), 0) AS total_gasto
+    FROM cards c
+    LEFT JOIN transactions t ON c.id = t.card_id
+    WHERE c.id = 1
+    GROUP BY c.id;
+
+# Calcular saldo disponível
+SELECT c.nome_cartao, c.limite, COALESCE(SUM(t.valor), 0) AS total_gasto, 
+(c.limite - COALESCE(SUM(t.valor), 0)) AS saldo_disponivel
+    FROM cards c
+    LEFT JOIN transactions t ON c.id = t.card_id
+    WHERE c.id = 1
+    GROUP BY c.id;
+
+# Calcular a percentagem usada
+SELECT c.nome_cartao, c.limite, COALESCE(SUM(t.valor), 0) AS total_gasto, 
+(c.limite - COALESCE(SUM(t.valor), 0)) AS saldo_disponivel,
+ROUND((COALESCE(SUM(t.valor), 0) / c.limite) * 100, 2) AS percentagem_usada
+    FROM cards c
+    LEFT JOIN transactions t ON c.id = t.card_id
+    WHERE c.id = 1
+    GROUP BY c.id;
+
+# Ver status de todos os cartões de um utilizador
+SELECT u.nome AS utilizador, c.nome_cartao, c.limite,
+  COALESCE(SUM(t.valor), 0) AS total_gasto,
+  (c.limite - COALESCE(SUM(t.valor), 0)) AS saldo_disponivel,
+  ROUND((COALESCE(SUM(t.valor), 0) / c.limite) * 100, 2) AS percentagem_usada
+FROM users u
+JOIN cards c ON u.id = c.user_id
+LEFT JOIN transactions t ON c.id = t.card_id
+WHERE u.id = 1
+GROUP BY c.id;
